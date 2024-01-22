4bca5f5948662759067d79ce2576b3579c0e48f8
foxstudent106232 <danielpyld.work@gmail.com>
2024-01-22 21:21:58 +0200

    Creates table school

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -4,20 +4,17 @@ DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS courses;
 
--- Create the 'groups' table
 CREATE TABLE IF NOT EXISTS groups (
                                       group_id SERIAL PRIMARY KEY,
                                       group_name VARCHAR(255) NOT NULL
 );
 
--- Create the 'courses' table
 CREATE TABLE IF NOT EXISTS courses (
                                        course_id SERIAL PRIMARY KEY,
                                        course_name VARCHAR(255) NOT NULL,
                                        course_description TEXT
 );
 
--- Create the 'students' table with a foreign key reference to 'groups'
 CREATE TABLE IF NOT EXISTS students (
                                         student_id SERIAL PRIMARY KEY,
                                         group_id INT,
@@ -26,7 +23,6 @@ CREATE TABLE IF NOT EXISTS students (
                                         FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
--- Create the 'student_courses' table with composite primary key and foreign keys
 CREATE TABLE IF NOT EXISTS student_courses (
                                                student_id INT,
                                                course_id INT,
