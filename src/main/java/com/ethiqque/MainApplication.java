05512a09cfa6315011888fa4096ef53cd942cfb2
foxstudent106232 <danielpyld.work@gmail.com>
2024-01-22 23:12:39 +0200

    Added InsertingInDB class

--- src/main/java/com/ethiqque/MainApplication.java
+++ src/main/java/com/ethiqque/MainApplication.java
@@ -2,10 +2,17 @@ package com.ethiqque;
 
 import com.ethiqque.util.DatabaseInitialization;
 
+import java.sql.SQLException;
+
 public class MainApplication {
 
     public static void main(String[] args) {
         DatabaseInitialization dbInit = new DatabaseInitialization();
         dbInit.initializeDatabase();
+        try {
+            InsertingInDB.insertData();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
