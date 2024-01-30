91fd464cbe54d11bba451348c83e317ce578c5ba
foxstudent106232 <danielpyld.work@gmail.com>
2024-01-30 14:29:02 +0200

    Correct working architecture

--- src/main/java/com/ethiqque/MainApplication.java
+++ src/main/java/com/ethiqque/MainApplication.java
@@ -19,20 +19,3 @@ public class MainApplication {
 
 
 
-
-
-//package com.ethiqque;
-//
-//import com.ethiqque.util.DatabaseConnection;
-//import com.ethiqque.util.DatabaseInitializer;
-//
-//import java.sql.SQLException;
-//
-//public class MainApplication {
-//
-//    public static void main(String[] args) throws SQLException {
-//        DatabaseInitializer.initialize();
-//        DataGenerator.generateTestData();
-//    }
-//}
-//
